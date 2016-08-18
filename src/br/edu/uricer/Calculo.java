4073243ecc50a54aace0724193bd0b6799775f8b
Marlon <marlon_kin@hotmail.com>
2016-08-18 20:45:46 -0300

    Primeira versão funcional.

--- src/br/edu/uricer/Calculo.java
+++ src/br/edu/uricer/Calculo.java
@@ -15,33 +15,44 @@ public class Calculo {
 
     
     public int calcular(){
-        double imcGeral = peso / Math.pow(estatura, 2);
+        double imcGeral = calcularImcGeral();
         
         if(sexo.equals("M")){
+            return calcularImcHomem(imcGeral);
+           
+        }else{
+            return calcularImcMulher(imcGeral);
+        }
+            
+    }
+    private double calcularImcGeral(){
+        return peso / Math.pow(estatura,2);
+    }
+    
+    private int calcularImcHomem(double imcGeral){
             int imc = 1;
             
             if(imcGeral < 20.7){
                 imc = 0;
-            }else if(imcGeral > 26.4){
+                }else if(imcGeral > 26.4){
                 imc = 2;   
-            }
-            return imc;
-           
-        }else{
-            if(sexo.equals("F")){
+                }
+                return imc;
+        
+    }
+    
+    private int calcularImcMulher(double imcGeral){
             int imc = 1;
             
             if(imcGeral < 19.1){
                 imc = 0;
-            }else if(imcGeral > 25.8){
+                }else if(imcGeral > 25.8){
                 imc = 2;   
-            }
-            return imc;
-            }
-            
-        }
-        
-        return 0;
+                }
+                return imc;
     }
-    
 }
+               
+
+
+    
